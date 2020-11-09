93acae9ca4ce9ea43379113269f31367f060a8b5
therealmarvin <nakedtoothbrush@hotmail.com>
2020-11-08 20:57:51 -0500

    function to test dynamic code loading

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+from python_file_loader import file_loader, call_method_on_module
+
 if __name__ == '__main__':
-    pass
+    file_loader("remote_code.py")
+    call_method_on_module("remote_code", "test1")
+    call_method_on_module("remote_code", "test2", {"arg": "some var"})
